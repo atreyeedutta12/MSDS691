fb339a690dc51438a01819189c270fe007c0eb85
atreyeedutta12 <atreyee.dutta@eastern.edu>
2025-07-09 13:01:09 -0400

    Update ml_model.py testing

--- ml_model.py
+++ ml_model.py
@@ -37,8 +37,11 @@ def predict_GovRevnGDP(GeoEco_stats, model):
     X=df.drop('governmentRevenueOfGdp1',axis=1)
     y=df['governmentRevenueOfGdp1']
     #results = []
-    from sklearn.model_selection import train_test_split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    #from sklearn.model_selection import train_test_split
+    #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    train_size = int(len(X) * 0.8) # Example: 80% for training
+    X_train, X_test = X[0:train_size], X[train_size:]
+    y_train, y_test = y[0:train_size], y[train_size:]
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
     return y_pred
